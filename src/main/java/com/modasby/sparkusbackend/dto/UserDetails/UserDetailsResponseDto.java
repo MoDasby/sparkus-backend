14da7013b5d53a212a97198790b6a47c12ea552a
MoDasby <giullianomendes033@gmail.com>
2024-05-05 17:16:25 -0300

    refactor: update entities

--- src/main/java/com/modasby/sparkusbackend/dto/UserDetails/UserDetailsResponseDto.java
+++ src/main/java/com/modasby/sparkusbackend/dto/UserDetails/UserDetailsResponseDto.java
@@ -18,17 +18,29 @@ public class UserDetailsResponseDto {
 
     private String username;
     private String name;
+
     @JsonProperty("icon_path")
     private String iconPath;
 
+    @JsonProperty("followers_count")
+    private long followersCount;
+
+    @JsonProperty("following_count")
+    private long followingsCount;
+
+    private boolean following;
+
     List<PostResponseDto> posts;
 
-    public UserDetailsResponseDto(User user) {
+    public UserDetailsResponseDto(User user, boolean following) {
         Function<Post, Boolean> isLiked = (p) -> user.getLikedPosts().contains(p);
 
         this.username = user.getUsername();
         this.name = user.getName();
         this.iconPath = user.getIconPath();
         this.posts = user.getPosts().stream().map(p -> new PostResponseDto(p, isLiked.apply(p))).collect(Collectors.toList());
+        this.followersCount = user.getFollowers().size();
+        this.followingsCount = user.getFollowing().size();
+        this.following = following;
     }
 }
