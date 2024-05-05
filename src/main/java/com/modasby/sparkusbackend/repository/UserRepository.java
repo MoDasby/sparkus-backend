219bcf60a31854f26af2868b8d81ea509ffc0137
MoDasby <giullianomendes033@gmail.com>
2024-05-05 17:16:18 -0300

    refactor: update entities

--- src/main/java/com/modasby/sparkusbackend/repository/UserRepository.java
+++ src/main/java/com/modasby/sparkusbackend/repository/UserRepository.java
@@ -3,12 +3,14 @@ package com.modasby.sparkusbackend.repository;
 import com.modasby.sparkusbackend.model.User;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 
     User findByEmail(String email);
 
